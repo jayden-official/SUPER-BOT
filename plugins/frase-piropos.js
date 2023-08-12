160f7e188324a52dde4bf0331c0f774ea6de2e3f
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-08-12 16:50:25 +0300

    Update frase-piropos.js

--- plugins/frase-piropos.js
+++ plugins/frase-piropos.js
@@ -1,14 +1,14 @@
 /* By https://github.com/DIEGO-OFC/DORRAT-BOT-MD */
 
-let handler = async (m, { conn, text}) => {
+let handler = async(m, { conn, text}) => {
 
-m.reply(`*╔═══════════════════════════*\n➢ *"${pickRandom(global.piropo)}"*\n*╚═══════════════════════════*`)
+m.reply(`*╔═══════════════════════════*\n➢ *"${pickRandom(global.compliment)}" *\n*╚═══════════════════════════*`)
 }
-handler.tags = ['frases']
-handler.command = ['piropo']
+handler.tags = ['phrases']
+handler.command = ['complimentary']
 export default handler
 
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]}
 
-global.piropo = ["Me gustaría ser papel para poder envolver ese bombón.", "Eres como wifi sin contraseña, todo el mundo te busca", "Quién fuera bus para andar por las curvas de tu corazón.", "Quiero volar sin alas y salir de este universo, entrar en el tuyo y amarte en silencio.", "Quisiera ser mantequilla para derretirme en tu arepa.", "Si la belleza fuera pecado vos ya estarías en el infierno.", "Me Gustaría Ser Un Gato Para Pasar 7 Vidas A Tu Lado.", "Robar Está Mal Pero Un Beso De Tu Boca Sí Me Lo Robaría.", "Qué Hermoso Es El Cielo Cuando Está Claro Pero Más Hermoso Es El Amor Cuando Te Tengo A Mi Lado.", "Bonita, Camina Por La Sombra, El Sol Derrite Los Chocolates.", "Si Fuera Un Correo Electrónico Serías Mi Contraseña.", "Quisiera que fueses monte para darte machete", "Perdí mi número de teléfono ¿Me das el tuyo?", "¿Cómo te llamas para pedirte de regalo a Santa Claus?", " En el cielo hay muchas estrellas, pero la más brillante está en la Tierra y eres tú.", "¿Acaba de salir el sol o es la sonrisa que me regalas hoy?", "No es el ron ni la cerveza, eres tú quien se me ha subido a la cabeza", "Si hablamos de matemáticas eres la suma de todos mis deseos.", "Pareces Google porque tienes todo lo que yo busco.", "Mi café favorito, es el de tus ojos.", "Quiero ser photoshop para retocarte todo el cuerpo.", "Quisiera que fueras cereal, para cucharearte en las mañanas.", "Quien fuera hambre, para darte tres veces al día."]
+global.compliro = ["I would like to be paper to be able to wrap that chocolate.", "You are like Wi-Fi without a password, everyone is looking for you", "Who was a bus to walk around the curves of your heart.", "I want to fly without wings and leave this universe, enter yours and love you in silence.", "I would like to be butter to melt in your arepa.", "If beauty were a sin, you would already be in hell.", "I would like to be A cat to spend 7 lives by your side.", "Stealing is wrong but a kiss from your mouth would steal it from me.", "How beautiful the sky is when it's clear, but love is more beautiful when I have you by my side .", "Pretty, Walk In The Shade, The Sun Melts The Chocolates.", "If It Were An Email You Would Be My Password.", "I would like you to go to the mountains to give you a machete", "I lost my phone number. Can you give me yours?", "What is your name to ask Santa Claus for a gift?", "There are many stars in the sky, but the brightest is on Earth and it's you.", "Did the sun just rise or Is it the smile that you give me today?", "It's not the rum or the beer, it's you who has gone to my head", "If we talk about mathematics you are the sum of all my desires.", "You look like Google because You have everything I'm looking for.", "My favorite coffee is the one with your eyes.", "I want to be photoshop to retouch your whole body.", "I wish you were cereal, to spoon you in the morning.", "Who were hungry, to give you three times a day."]
