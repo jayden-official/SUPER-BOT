26529237ef95c476ca71821af2ff11451d3dbfd6
KHALID NICCUR <113862545+Khalid-niccur@users.noreply.github.com>
2023-08-12 08:35:56 +0300

    Update convertidor-tovideo.js

--- plugins/convertidor-tovideo.js
+++ plugins/convertidor-tovideo.js
@@ -1,9 +1,9 @@
 import { webp2mp4 } from '../lib/webp2mp4.js'
 import { ffmpeg } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝚅𝙸𝙳𝙴𝙾 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`
+if (!m.quoted) throw `*[❗𝐈𝐍𝐅𝐎❗] RESPOND TO A STICKER TO BE CONVERTED TO VIDEO, USING THE COMMNAND ${usedPrefix + command}*`
 let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝚅𝙸𝙳𝙴𝙾 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`
+if (!/webp/.test(mime)) throw `*[❗𝐈𝐍𝐅𝐎❗] RESPOND TO A STICKER TO BE CONVERTED TO VIDEO, USING THE COMMNAND ${usedPrefix + command}*`
 let media = await m.quoted.download()
 let out = Buffer.alloc(0)
 if (/webp/.test(mime)) {
