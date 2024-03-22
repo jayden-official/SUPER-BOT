7aa0a6cfbc2229d9698a43fdaa9ad0324d38d8fa
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-03-22 17:41:27 +0300

    Delete plugins directory

--- plugins/_antitag.js
+++
@@ -1,7 +0,0 @@
-const handler = (m) => m;
-
-handler.before = async function(m) {
-  if (m.mentionedJid.length >= 20) await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-};
-
-export default handler;
--- plugins/downloader-pptiktok.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args, text }) => {
-if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] enter the name or the username of the tiktok*'
-let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkeysapi}`
-conn.sendFile(m.chat, res, 'error.jpg', `*[ ✔ ]  here is the profile photo of${text}*`, m, false)}
-handler.help = ['tiktokfoto'].map(v => v + ' <username>')
-handler.tags = ['downloader']
-handler.command = /^(tiktokfoto|pptiktok)$/i
-export default handler
