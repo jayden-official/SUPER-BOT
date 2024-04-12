7b58070bbc7727fcf9dcd5916e6159c18897f0b9
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-04-12 16:07:03 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1425,7 +1425,7 @@ export async function participantsUpdate({id, participants, action}) {
       if (chat.welcome && !chat?.isBanned) {
         const groupMetadata = await m.conn.groupMetadata(id) || (conn.chats[id] || {}).metadata;
         for (const user of participants) {
-          let pp = './src/avatar_contact.png';
+          let pp = '';
           try {
             pp = await m.conn.profilePictureUrl(user, 'image');
           } catch (e) {
