2df6ae8d4d8974c3d367769137d554b3a190840a
Jayden-official <pancafe198@gmail.com>
2024-06-26 17:55:00 +0300

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-let love = `https://github.com/Khalid-official *❤️❤️ LOVE METER ❤️❤️*
+let love = `https://github.com/jayden-official *❤️❤️ LOVE METER ❤️❤️*
 *The love for ${text} For you it is* *${Math.floor(Math.random() * 100)}%* *of a 100%*
 *You should ask her to be your girlfriend*
 `.trim()
