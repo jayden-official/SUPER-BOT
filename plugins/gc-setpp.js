9a423077763cf1eb6016d8eba6112d001305aa7a
Jayden-official <pancafe198@gmail.com>
2024-06-26 19:21:28 +0300

    Update gc-setpp.js

--- plugins/gc-setpp.js
+++ plugins/gc-setpp.js
@@ -3,9 +3,9 @@ let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/image/.test(mime)) {
 let img = await q.download()
-if (!img) throw 'https://github.com/Khalid-official *⚠️️ Respond to an image.*'
+if (!img) throw 'https://github.com/jayden-official *⚠️️ Respond to an image.*'
 await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('https://github.com/Khalid-official ⚘ *_Picture updated successfully._*'))
-} else throw 'https://github.com/Khalid-official *⚠️️ Respond to an image.*'}
+} else throw 'https://github.com/jayden-official *⚠️️ Respond to an image.*'}
 handler.command = /^setpp(group|grup|gc)?$/i
 handler.group = true
 handler.admin = true
