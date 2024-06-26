fb7cb610bc4d1aca68810cd6bd15b1020e5c5983
Jayden-official <pancafe198@gmail.com>
2024-06-26 17:35:07 +0300

    Update downloader-twitter.js

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import cheerio from 'cheerio'
 import vm from 'node:vm'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] enter a Twitter link, example: ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw` 
+if (!text) throw `https://github.com/jayden-official *[❗𝐈𝐍𝐅𝐎❗] enter a Twitter link, example: ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw` 
 try {
 let res = await twitterDl(text)
 await m.reply(global.wait)
@@ -15,13 +15,13 @@ try {
 const AA = await savefrom(text)    
 conn.sendFile(m.chat, AA.url[0].url, 'error.mp4', '*here is the video*', m)    
 } catch {
-throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, please try again later*'  
+throw 'https://github.com/jayden-official *[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, please try again later*'  
 }}}
 handler.command = /^((twdl|tw|twt|twitter)(dl)?)$/i
 export default handler
 async function twitterDl(url) {
 let id = /twitter\.com\/[^/]+\/status\/(\d+)/.exec(url)?.[1]
-if (!id) throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] enter a valid twitter link, example: #twitter* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw'
+if (!id) throw 'https://github.com/jayden-official *[❗𝐈𝐍𝐅𝐎❗] enter a valid twitter link, example: #twitter* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw'
 let res = await fetch(`https://tweetpik.com/api/tweets/${id}`)
 if (res.status !== 200) throw res.statusText
 let json = await res.json()
@@ -36,7 +36,7 @@ media.push({ url: vid.url, type: i.type })
 media.push({ url: i.url, type: i.type })}}
 return {
 caption: json.text, media 
-}} else throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, please try again later *' }
+}} else throw 'https://github.com/jayden-official *[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, please try again later *' }
 async function savefrom(urlL) {
 let body = new URLSearchParams({ "sf_url": encodeURI(urlL), "sf_submit": "", "new": 2, "lang": "id", "app": "", "country": "id", "os": "Windows", "browser": "Chrome", "channel": " main", "sf-nomad": 1 });
 let { data } = await axios({ "url": "https://worker.sf-tools.com/savefrom.php", "method": "POST", "data": body, "headers": { "content-type": "application/x-www-form-urlencoded", "origin": "https://id.savefrom.net", "referer": "https://id.savefrom.net/", "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36" }});
