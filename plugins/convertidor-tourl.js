4bc79e309b83fea29bad3ea08cd3ca537ec78e64
Jayden-official <pancafe198@gmail.com>
2024-06-26 16:43:40 +0300

    Update convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -3,11 +3,11 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw 'https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] RESPOND TO AN IMAGE OR AUDIO TO BE CONVERTED TO A LINK'
+if (!mime) throw 'https://github.com/jayden-official *[❗𝐈𝐍𝐅𝐎❗] RESPOND TO AN IMAGE OR AUDIO TO BE CONVERTED TO A LINK'
 let media = await q.download()
 let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 let link = await (isTele ? uploadImage : uploadFile)(media)
-m.reply(`*RESULTS🐝:* ${link}`)
+m.reply(`*RESULTS:* ${link}`)
 }
 handler.help = ['tourl <reply image>']
 handler.tags = ['sticker']
