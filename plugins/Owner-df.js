e018126043eb251fa882eb634a4226b812bc5326
Jayden-official <pancafe198@gmail.com>
2024-06-26 15:35:54 +0300

    Update Owner-df.js

--- plugins/Owner-df.js
+++ plugins/Owner-df.js
@@ -16,7 +16,7 @@ let ar = Object.keys(plugins)
     if (!ar1.includes(args[0])) return m.reply(`*🗃️ NOT FOUND!*\n==================================\n\n${ar1.map(v => ' ' + v).join`\n`}`)
 const file = join(__dirname, '../plugins/' + args[0] + '.js')
 unlinkSync(file)
-conn.reply(m.chat, `https://github.com/Khalid-official Succes deleted "plugins/${args[0]}.js"`, m)
+conn.reply(m.chat, `https://github.com/jayden-official Succes deleted "plugins/${args[0]}.js"`, m)
     
 }
 handler.help = ['df']
