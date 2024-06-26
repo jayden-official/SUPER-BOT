718700dfc61c26c4df616e82c88222e44bf8dcb7
Jayden-official <pancafe198@gmail.com>
2024-06-26 17:54:02 +0300

    Update fun-hate.js

--- plugins/fun-hate.js
+++ plugins/fun-hate.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command, text }) => {
-let love = `https://github.com/Khalid-official  *😡😡️ HATE METER 😡😡*
+let love = `https://github.com/jayden-official  *😡😡️ HATE METER 😡😡*
 *The hate for ${text} For you it is* *${Math.floor(Math.random() * 100)}%* *of a 100%*
 *You should ask him/her the reasom for the hate*
 `.trim()
