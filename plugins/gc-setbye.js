57d6f326ae8534a0a4869ef39b4c3bdad281db50
Jayden-official <pancafe198@gmail.com>
2024-06-26 19:18:24 +0300

    Update gc-setbye.js

--- plugins/gc-setbye.js
+++ plugins/gc-setbye.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
    if (text) {
      global.db.data.chats[m.chat].sBye = text
-     m.reply('https://github.com/Khalid-official ✅ Goodbye message set')
+     m.reply('https://github.com/jayden-official ✅ Goodbye message set')
    } else throw `✳️ Enter the message\n@user (mention)`
 }
 handler.help = ['setbye <text>']
