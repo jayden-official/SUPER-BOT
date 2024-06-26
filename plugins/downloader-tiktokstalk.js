53aa6896bf4756b90c18b8681e01cfc4a1e5c7ca
Jayden-official <pancafe198@gmail.com>
2024-06-26 17:32:43 +0300

    Update downloader-tiktokstalk.js

--- plugins/downloader-tiktokstalk.js
+++ plugins/downloader-tiktokstalk.js
@@ -2,7 +2,7 @@
 import fg from 'api-dylux'
 let handler = async (m, { conn, text, args }) => {
 	
-  if (!text) throw `https://github.com/Khalid-official ✳️Enter the Username of a TikTok user `
+  if (!text) throw `https://github.com/jayden-official ✳️Enter the Username of a TikTok user `
   let res = await fg.ttStalk(args[0])
   let txt = `
 ┌──「 *TIKTOK STALK* 
