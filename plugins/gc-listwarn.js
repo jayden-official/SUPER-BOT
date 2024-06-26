053ef334005834d1d07233bc63d0bf683555265e
Jayden-official <pancafe198@gmail.com>
2024-06-26 19:10:18 +0300

    Update gc-listwarn.js

--- plugins/gc-listwarn.js
+++ plugins/gc-listwarn.js
@@ -1,13 +1,13 @@
 let handler = async(m, { conn, args, groupMetadata}) => {
         let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-        if (!(who in global.db.data.users)) throw `https://github.com/Khalid-official ✳️ The user is not found in my database`
+        if (!(who in global.db.data.users)) throw `https://github.com/jayden-official ✳️ The user is not found in my database`
         let warn = global.db.data.users[who].warn
         let name = conn.getName(who)
        m.reply(`
   *WARNINGS*
 
-🐝 *Name :* ${name}
-🐝 *Warns :* ${warn}`)
+ *Name :* ${name}
+ *Warns :* ${warn}`)
 }
 
 handler.help = ['warns']
