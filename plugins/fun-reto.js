56fd2c421caa0d480e4a9cbcb0a078e456f9e76a
Jayden-official <pancafe198@gmail.com>
2024-06-26 18:00:35 +0300

    Update fun-reto.js

--- plugins/fun-reto.js
+++ plugins/fun-reto.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*┌────「 𝚁𝙴𝚃𝙾 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 bumblebee 」─*`, m)
+conn.reply(m.chat,`*┌────「 𝚁𝙴𝚃𝙾 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 super 」─*`, m)
 }
 handler.help = ['reto']
 handler.tags = ['fun']
