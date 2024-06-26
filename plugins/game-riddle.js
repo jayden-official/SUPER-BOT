83550da1a011419758946651a345d2763087de86
Jayden-official <pancafe198@gmail.com>
2024-06-26 18:23:23 +0300

    Update game-riddle.js

--- plugins/game-riddle.js
+++ plugins/game-riddle.js
@@ -5,14 +5,14 @@ let handler = async (m, { conn, usedPrefix }) => {
 conn.tekateki = conn.tekateki ? conn.tekateki : {}
 let id = m.chat
 if (id in conn.tekateki) {
-conn.reply(m.chat, 'https://github.com/Khalid-official *‼️THERE ARE STILL UNANSWERED RIDDLES IN THE CHAT‼️*', conn.tekateki[id][0])
+conn.reply(m.chat, 'https://github.com/jayden-official *‼️THERE ARE STILL UNANSWERED RIDDLES IN THE CHAT‼️*', conn.tekateki[id][0])
 throw false
 }
 let tekateki = JSON.parse(fs.readFileSync(`./src/game/riddle.json`))
 let json = tekateki[Math.floor(Math.random() * tekateki.length)]
 let _clue = json.response
 let clue = _clue.replace(/[A-Za-z]/g, '_')
-let caption = `https://github.com/Khalid-official 
+let caption = `https://github.com/jayden-official 
 ⷮ *${json.question}*
 *• TIME⌚:* ${(timeout / 1000).toFixed(2)} seconds 
 *• BONUS🎗️:* +${poin} Exp
@@ -21,7 +21,7 @@ conn.tekateki[id] = [
 await conn.reply(m.chat, caption, m), json, 
 poin,
 setTimeout(async () => {
-if (conn.tekateki[id]) await conn.reply(m.chat, `https://github.com/Khalid-official *TIME IS OVER BLUD*😑!\n*Answer:* ${json.response}`, conn.tekateki[id][0])
+if (conn.tekateki[id]) await conn.reply(m.chat, `https://github.com/jayden-official *TIME IS OVER BLUD*😑!\n*Answer:* ${json.response}`, conn.tekateki[id][0])
 delete conn.tekateki[id]
 }, timeout)]}
 handler.help = ['acertijo']
