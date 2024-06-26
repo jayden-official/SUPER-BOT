679e122dace4b94f678c2c44fb3bbac23b758c70
Jayden-official <pancafe198@gmail.com>
2024-06-26 18:10:30 +0300

    Update game-delttt.js

--- plugins/game-delttt.js
+++ plugins/game-delttt.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn, text }) => {
 let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
 if (room == undefined) return conn.reply(m.chat,`https://github.com/Khalid-official ✳️ You are not in a TicTacToe game 🎮 `, m)
 delete conn.game[room.id]
-await conn.reply(m.chat, `https://github.com/Khalid-official ✅ *tictactoe session restarted 🎮*`, m)
+await conn.reply(m.chat, `https://github.com/jayden-official ✅ *tictactoe session restarted 🎮*`, m)
 }
 handler.help = ['delttt']
 handler.tags = ['game']
