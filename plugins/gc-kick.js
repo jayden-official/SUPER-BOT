2d364315c3dfe1ba9087529a4c8e3447e1ea920c
Jayden-official <pancafe198@gmail.com>
2024-06-26 19:00:05 +0300

    Update gc-kick.js

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kickte = `https://github.com/Khalid-official ✳️ Correct use of command\n*${usedPrefix + command}* @tag`
+let kickte = `https://github.com/jayden-official ✳️ Correct use of command\n*${usedPrefix + command}* @tag`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)})
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`https://github.com/Khalid-official ✅ user successfully removed💚`)
+m.reply(`https://github.com/jayden-official ✅ user successfully removed💚`)
 
 }
 
