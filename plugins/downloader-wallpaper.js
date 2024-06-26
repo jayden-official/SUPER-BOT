9327b004f1818ffc51bdc3b931460affeec5a753
Jayden-official <pancafe198@gmail.com>
2024-06-26 17:36:06 +0300

    Update downloader-wallpaper.js

--- plugins/downloader-wallpaper.js
+++ plugins/downloader-wallpaper.js
@@ -1,6 +1,6 @@
 import { wallpaper, wallpaperv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] example  use the comand ${usedPrefix + command} Minecraft*`
+if (!text) throw `https://github.com/jayden-official *[❗𝐈𝐍𝐅𝐎❗] example  use the comand ${usedPrefix + command} Minecraft*`
 const res = await (/2/.test(command) ? wallpaperv2 : wallpaper)(text)
 const img = res[Math.floor(Math.random() * res.length)]
 conn.sendFile(m.chat, img, 'error.jpg', `*the results found  ${text}*`, m)
