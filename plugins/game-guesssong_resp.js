76744cffeb00155bbfa0dbce9e9721cb493cceda
Jayden-official <pancafe198@gmail.com>
2024-06-26 18:19:17 +0300

    Update game-guesssong_resp.js

--- plugins/game-guesssong_resp.js
+++ plugins/game-guesssong_resp.js
@@ -10,11 +10,11 @@ if (m.quoted.id == this.tebaklagu[id][0].id) {
 let json = JSON.parse(JSON.stringify(this.tebaklagu[id][1]))
 if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
 global.db.data.users[m.sender].exp += this.tebaklagu[id][2]
-m.reply(`https://github.com/Khalid-official ✅*CORRECT💯*!\n+${this.tebaklagu[id][2]} XP`)
+m.reply(`https://github.com/jayden-official ✅*CORRECT💯*!\n+${this.tebaklagu[id][2]} XP`)
 clearTimeout(this.tebaklagu[id][3])
 delete this.tebaklagu[id]
 } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Cashiers!`)
-else m.reply(`https://github.com/Khalid-official ❌*INCORRECT*!`)
+else m.reply(`https://github.com/jayden-official ❌*INCORRECT*!`)
 }
 return !0
 },
