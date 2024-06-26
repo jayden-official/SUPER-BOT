ce9e57e66a7ea3183ef23ac75ea68b8e8859a9ba
Jayden-official <pancafe198@gmail.com>
2024-06-26 16:38:01 +0300

    Update convertidor-toanime.js

--- plugins/convertidor-toanime.js
+++ plugins/convertidor-toanime.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ""
 if (!/image/g.test(mime)) throw '*[❗] Reply or tag an image*'
-m.reply('https://github.com/Khalid-official *[❗] Convert image to anime design, be patient as I send the result*')    
+m.reply('https://github.com/jayden-official *[❗] Convert image to anime design, be patient as I send the result*')    
 let data = await q.download?.()
 let image = await uploadImage(data)
 try {
@@ -18,7 +18,7 @@ try{
 let anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`
 await conn.sendFile(m.chat, anime3, 'error.jpg', null, m) 
 } catch (e) {
-throw 'https://github.com/Khalid-official *[❗] Error, check eye on the image. Be visible A person's face*'
+throw 'https://github.com/jayden-official *[❗] Error, check eye on the image. Be visible A person's face*'
 }}}}
 handler.help = ["toanime"]
 handler.tags = ["tools"]
