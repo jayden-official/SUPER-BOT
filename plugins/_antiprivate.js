7e3f334d55d307e12b0967f94cb467bd2aa60b93
Jayden-official <pancafe198@gmail.com>
2024-06-26 16:11:28 +0300

    Update _antiprivate.js

--- plugins/_antiprivate.js
+++ plugins/_antiprivate.js
@@ -8,7 +8,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`https://github.com/Khalid-official _*< ANTI-PRIVATE />*_\n\n*[ ℹ️ ] The anti-private feature is enabled, therefore you will be blocked.*`, false, {mentions: [m.sender]});
+    await m.reply(`https://github.com/jayden-official _*< ANTI-PRIVATE />*_\n\n*[ ℹ️ ] The anti-private feature is enabled, therefore you will be blocked.*`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
