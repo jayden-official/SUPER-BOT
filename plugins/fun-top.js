d742015b001a0e7434e8ba7d8798be869d80f839
Jayden-official <pancafe198@gmail.com>
2024-06-26 18:04:45 +0300

    Update fun-top.js

--- plugins/fun-top.js
+++ plugins/fun-top.js
@@ -2,7 +2,7 @@ import util from 'util'
 import path from 'path'
 let user = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) throw `https://github.com/Khalid-official EXAMPLE :\n.top *texto*`
+if (!text) throw `https://github.com/jayden-official EXAMPLE :\n.top *texto*`
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
 let b = ps.getRandom()
