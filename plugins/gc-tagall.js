0020f43670df7f69642df6499f5181dc642c54cf
Jayden-official <pancafe198@gmail.com>
2024-06-27 06:47:06 +0300

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,15 +5,15 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*MESSENGER:* ${pesan}`
-let teks = `*https://github.com/Khalid-official ⺀̷T̷R̷A̷N̷S̷F̷O̷R̷M̷E̷R̷S̷🐝̷G̷R̷O̷U̷P⺀*\n\n�? ${oi}\n\n�? *HANG TAGS:*\n`
+let teks = `*https://github.com/jayden-official âº€Ì·TÌ·RÌ·AÌ·NÌ·SÌ·FÌ·OÌ·RÌ·MÌ·EÌ·RÌ·SÌ·ðŸÌ·GÌ·RÌ·OÌ·UÌ·Pâº€*\n\nâ? ${oi}\n\nâ? *HANG TAGS:*\n`
 for (let mem of participants) {
-teks += `┣➥ @${mem.id.split('@')[0]}\n`}
-teks += `*�?* 𝐁𝐘 𝐁𝐔𝐌𝐁𝐋𝐄𝐁𝐄𝐄🐝𝐁𝐎𝐓 \n\n*▌│█║▌║▌║║▌║▌║▌║█*`
+teks += `â”£âž¥ @${mem.id.split('@')[0]}\n`}
+teks += `*â”?* ðð˜ ðð”ðŒðð‹ð„ðð„ð„ðŸððŽð“ \n\n*â–Œâ”‚â–ˆâ•‘â–Œâ•‘â–Œâ•‘â•‘â–Œâ•‘â–Œâ•‘â–Œâ•‘â–ˆ*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(tagall|invocar|invocacion|todos|invocaciÃ³n)$/i
 handler.admin = true
 handler.group = true
 export default handler
