27ad2016ee299861245f75291ae3258e5b0b3b8a
Jayden-official <pancafe198@gmail.com>
2024-06-27 08:04:09 +0300

    Update menu-animes.js

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -19,7 +19,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
-║    ◉— *𝗧𝗵𝗲 𝗕𝘂𝗺𝗯𝗹𝗲😘𝗯𝗲𝗲 𝗯𝗼𝘁* —◉
+║    ◉— *𝗧𝗵𝗲 𝘀𝘂𝗽𝗲𝗿😘 𝗯𝗼𝘁* —◉
 ║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
 ║➤ *𝗛ola, ${taguser}*
 ╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
