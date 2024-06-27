4870f7b2477975a250d65568e9e1b1e9c1d9edc9
Jayden-official <pancafe198@gmail.com>
2024-06-27 16:41:49 +0300

    Update owner-add_del_owner.js

--- plugins/owner-add_del_owner.js
+++ plugins/owner-add_del_owner.js
@@ -1,5 +1,5 @@
 const handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  const why = `https://github.com/Khalid-official *[❗] 
+  const why = `https://github.com/jayden-official *[❗] 
 Incorrect use of the command.*\n\n*—◉ Example of valid use:*\n*◉ ${usedPrefix + command} @${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} ${m.sender.split('@')[0]}*\n*◉ ${usedPrefix + command} <message>*`;
   const who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   if (!who) return conn.reply(m.chat, why, m, {mentions: [m.sender]});
@@ -15,9 +15,9 @@ New number successfully added to the owners list.*', m);
       const index = global.owner.findIndex(owner => owner[0] === numeroAEliminar);
       if (index !== -1) {
         global.owner.splice(index, 1);
-        await conn.reply(m.chat, 'https://github.com/Khalid-official *[❗]  The number was successfully removed from the owners list.*', m);
+        await conn.reply(m.chat, 'https://github.com/jayden-official *[❗]  The number was successfully removed from the owners list.*', m);
       } else {
-        await conn.reply(m.chat, 'https://github.com/Khalid-official *[❗] The number entered does not exist in the owners list.*', m);
+        await conn.reply(m.chat, 'https://github.com/jayden-official *[❗] The number entered does not exist in the owners list.*', m);
       }
       break;
   }
