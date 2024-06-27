6d34a98b49a80ec800bc9ef705c76a6c154de40b
Jayden-official <pancafe198@gmail.com>
2024-06-27 17:01:12 +0300

    Update owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -1,15 +1,15 @@
 let handler = async(m, { conn }) => {
 
 await conn.fetchBlocklist().then(async data => {
-let txt = `https://github.com/Khalid-official *≡ Block List*\n\n*Total :* ${data.length}\n\n┌─⊷\n`
+let txt = `https://github.com/jayden-official *≡ Block List*\n\n*Total :* ${data.length}\n\n┌─⊷\n`
 for (let i of data) {
-txt += `🐝 @${i.split("@")[0]}\n`
+txt += `  @${i.split("@")[0]}\n`
 }
 txt += "└───────────"
 return conn.reply(m.chat, txt, m, { mentions: await conn.parseMention(txt) })
 }).catch(err => {
 console. log(err);
-throw 'https://github.com/Khalid-official No numbers blocked'
+throw 'https://github.com/jayden-official No numbers blocked'
 })
 }
 
