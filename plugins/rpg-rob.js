d22b615758269f432393e6c9515cd6994e4cca60
Jayden-official <pancafe198@gmail.com>
2024-06-27 18:08:01 +0300

    Update rpg-rob.js

--- plugins/rpg-rob.js
+++ plugins/rpg-rob.js
@@ -2,19 +2,19 @@
 let ro = 3000
 let handler = async (m, { conn, usedPrefix, command}) => {
     let time = global.db.data.users[m.sender].lastrob + 7200000
-    if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `https://github.com/Khalid-official ⏱️Hey! Wait *${msToTime(time - new Date())}* to steal again`
+    if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `https://github.com/jayden-official ⏱️Hey! Wait *${msToTime(time - new Date())}* to steal again`
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-     if (!who) throw `https://github.com/Khalid-official ✳️ Tag someone to steal`
-     if (!(who in global.db.data.users)) throw `https://github.com/Khalid-official ✳️ The user is not found in my database`
+     if (!who) throw `https://github.com/jayden-official ✳️ Tag someone to steal`
+     if (!(who in global.db.data.users)) throw `https://github.com/jayden-official ✳️ The user is not found in my database`
     let users = global.db.data.users[who]
     let rob = Math.floor(Math.random() * ro)
     if (users.exp < rob) return m.reply(`🔖 @${who.split`@`[0]} has less than *${ro} xp*\nDon't steal from a mightyv":`, null, { mentions: [who] })    
    global.db.data.users[m.sender].exp += rob
    global.db.data.users[who].exp -= rob
   
-    m.reply(`https://github.com/Khalid-official 
+    m.reply(`https://github.com/jayden-official 
   ‣ you stole *${rob} XP* a @${who.split`@`[0]}
   `, null, { mentions: [who] })
     global.db.data.users[m.sender].lastrob = new Date * 1
