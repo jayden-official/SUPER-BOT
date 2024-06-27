8477803ac38a8ac2126d1efb2cfb41597b8700ea
Jayden-official <pancafe198@gmail.com>
2024-06-27 16:35:59 +0300

    Update mipilot-stopbot.js

--- plugins/mipilot-stopbot.js
+++ plugins/mipilot-stopbot.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 async function handler(m, {conn, usedPrefix}) {
-   if (conn.user.jid == global.conn.user.jid) return m.reply(`https://github.com/Khalid-official *[❗] No puedes apagar el Bot principal.*`)
-   m.reply(`https://github.com/Khalid-official *[❗] Dejarás de ser un Sub Bot en 5 segundos...*`)
+   if (conn.user.jid == global.conn.user.jid) return m.reply(`https://github.com/jayden-official *[❗] No puedes apagar el Bot principal.*`)
+   m.reply(`https://github.com/jayden-official *[❗] Dejarás de ser un Sub Bot en 5 segundos...*`)
    conn.fstop = true
    conn.ws.close()
 }
