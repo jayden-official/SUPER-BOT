029f484d88ba250ca9f9b4e8e6886cf33903d92c
Jayden-official <pancafe198@gmail.com>
2024-06-27 21:26:54 +0300

    Update tools-translate.js

--- plugins/tools-translate.js
+++ plugins/tools-translate.js
@@ -1,7 +1,7 @@
 import translate from '@vitalets/google-translate-api'
 import fetch from 'node-fetch'
 let handler = async (m, { args, usedPrefix, command }) => {
-let msg = `https://github.com/Khalid-official *[❗𝐈𝐍𝐅𝐎❗] correct use of command  ${usedPrefix + command} (idioma) (texto)*\n*example:*\n*${usedPrefix + command} en Hello*\n\n*𝙲𝙾𝙽𝙾𝙲𝙴 𝙻𝙾𝚂 𝙸𝙳𝙸𝙾𝙼𝙰𝚂 𝙰𝙳𝙼𝙸𝚃𝙸𝙳𝙾𝚂 𝙴𝙽:*\n*- https://cloud.google.com/translate/docs/languages*`
+let msg = `https://github.com/jayden-official *[❗𝐈𝐍𝐅𝐎❗] correct use of command  ${usedPrefix + command} (idioma) (texto)*\n*example:*\n*${usedPrefix + command} en Hello*\n\n*𝙲𝙾𝙽𝙾𝙲𝙴 𝙻𝙾𝚂 𝙸𝙳𝙸𝙾𝙼𝙰𝚂 𝙰𝙳𝙼𝙸𝚃𝙸𝙳𝙾𝚂 𝙴𝙽:*\n*- https://cloud.google.com/translate/docs/languages*`
 if (!args || !args[0]) return m.reply(msg)  
 let lang = args[0]
 let text = args.slice(1).join(' ')
@@ -12,7 +12,7 @@ text = args.join(' ')}
 if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 try {      
 let result = await translate(`${text}`, { to: lang, autoCorrect: true })
-await m.reply('https://github.com/Khalid-official *translation:* ' + result.text)
+await m.reply('https://github.com/jayden-official *translation:* ' + result.text)
 } catch {
 try {    
 let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=${lolkeysapi}&text=${text}`)
