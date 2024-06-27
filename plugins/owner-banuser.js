de25f165c4c77fff562dd4432ba61e7dcc4acb82
Jayden-official <pancafe198@gmail.com>
2024-06-27 16:55:02 +0300

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -5,10 +5,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
      if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
      else who = m.chat
      let user = global.db.data.users[who]
-     if (!who) throw `https://github.com/Khalid-official ✳💚🧡Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
+     if (!who) throw `https://github.com/jayden-official ✳💚🧡Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
      let users = global.db.data.users
      users[who].banned = true
-     conn.reply(m.chat, `https://github.com/Khalid-official 
+     conn.reply(m.chat, `https://github.com/jayden-official 
 ✅ BANNED
 
 ───────────
