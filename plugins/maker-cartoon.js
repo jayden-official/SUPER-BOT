36ec90573110f12c2654c350db5a6694a55e488b
Jayden-official <pancafe198@gmail.com>
2024-06-27 07:54:45 +0300

    Update maker-cartoon.js

--- plugins/maker-cartoon.js
+++ plugins/maker-cartoon.js
@@ -105,10 +105,10 @@ function randomId() {
 let handler = async (m, { conn, usedPrefix, command }) => {
 	conn.cartoon = conn.cartoon ? conn.cartoon : {};
 	if (m.sender in conn.cartoon)
-		throw "https://github.com/Khalid-official There is still an unfinished process, my friend. Please wait until it's over. >//<";
+		throw "https://github.com/jayden-official There is still an unfinished process, my friend. Please wait until it's over. >//<";
 	let q = m.quoted ? m.quoted : m;
 	let mime = (q.msg || q).mimetype || q.mediaType || "";
-	if (!mime) throw `https://github.com/Khalid-official Where is the picture you want to convert to a cartoon?`;
+	if (!mime) throw `https://github.com/jayden-official Where is the picture you want to convert to a cartoon?`;
 	if (!/image\/(jpe?g|png)/.test(mime)) throw `file ${mime} not supported`;
 	else conn.cartoon[m.sender] = true;
 	m.reply("converting the picture to cartoon");
@@ -120,7 +120,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 					m.chat,
 					response.download.full,
 					"",
-					"https://github.com/Khalid-official The operation was successful♥  >//<",
+					"https://github.com/jayden-official The operation was successful♥  >//<",
 					m
 				);
 				let name = await conn.getName(m.sender),
@@ -136,12 +136,12 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 				conn.sendMessage(m.chat, await sticker.toMessage(), { quoted: m });
 			} else {
 				m.reply(
-					"https://github.com/Khalid-official Excuse me my friend, the picture does not reveal a face, please send a picture in which the face is exposed and visible."
+					"https://github.com/jayden-official Excuse me my friend, the picture does not reveal a face, please send a picture in which the face is exposed and visible."
 				);
 			}
 		});
 	} catch {
-		m.reply("https://github.com/Khalid-official Process failed :(");
+		m.reply("https://github.com/jayden-official Process failed :(");
 	} finally {
 		conn.cartoon[m.sender] ? delete conn.cartoon[m.sender] : false;
 	}
