0745f11ba21fc09a6f6db87748ff2fcff84bfeb0
Jayden-official <pancafe198@gmail.com>
2024-06-27 17:21:26 +0300

    Update owner-getsessi.js

--- plugins/owner-getsessi.js
+++ plugins/owner-getsessi.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
-    m.reply('https://github.com/Khalid-official *[❗] Please wait, Downloading the bot session file*')
-    let sesi = await fs.readFileSync('./BumbleSession/creds.json')
+    m.reply('https://github.com/jayden-official *[❗] Please wait, Downloading the bot session file*')
+    let sesi = await fs.readFileSync('./SuperSession/creds.json')
     return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'creds.json' }, { quoted: m })
 }
 handler.help = ['getsessi']
