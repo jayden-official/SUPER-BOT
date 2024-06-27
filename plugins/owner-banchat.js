b4b3f09bdce0cd967e42600256bedc526784dc5e
Jayden-official <pancafe198@gmail.com>
2024-06-27 16:52:19 +0300

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -3,7 +3,7 @@
 let handler = async(m, { conn, isOwner, isAdmin, isROwner }) => {
      if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
      global.db.data.chats[m.chat].isBanned = true
-     m.reply(' https://github.com/Khalid-official 🍫The Bot is successfully deactivated in this group, only Admins can use the bot🔒')
+     m.reply(' https://github.com/jayden-official 🍫The Bot is successfully deactivated in this group, only Admins can use the bot🔒')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
