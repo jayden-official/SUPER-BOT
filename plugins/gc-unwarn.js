ae39df8eab9168c1a747bb47f4f7c1e1b363c69e
Jayden-official <pancafe198@gmail.com>
2024-06-27 06:50:13 +0300

    Update gc-unwarn.js

--- plugins/gc-unwarn.js
+++ plugins/gc-unwarn.js
@@ -2,18 +2,18 @@ let handler = async(m, { conn, args, groupMetadata}) => {
          let who
          if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
          else who = m.chat
-         if (!who) throw `https://github.com/Khalid-official ✳️ Tag or mention someone`
-         if (!(who in global.db.data.users)) throw `https://github.com/Khalid-official ✳️ The user is not found in my database`
+         if (!who) throw `https://github.com/jayden-official ✳️ Tag or mention someone`
+         if (!(who in global.db.data.users)) throw `https://github.com/jayden-official ✳️ The user is not found in my database`
         let warn = global.db.data.users[who].warn
         if (warn > 0) {
           global.db.data.users[who].warn -= 1
-          m.reply(`https://github.com/Khalid-official ⚠️ *DELWARN*
-         
-🐝 Warnings: *-1*
-🐝 Total Warns: *${warn - 1}*`)
+          m.reply(`https://github.com/jayden-official ⚠️ *DELWARN*
+          
+ Warnings: *-1*
+ Total Warns: *${warn - 1}*`)
           m.reply(`✳️ An admin lowered his warning, now you have *${warn - 1}*`, who)
           } else if (warn == 0) {
-             m.reply('https://github.com/Khalid-official ✳️ The user has no warning')
+             m.reply('https://github.com/jayden-official ✳️ The user has no warning')
          }
 
 }
