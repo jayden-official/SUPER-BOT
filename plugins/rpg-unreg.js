92431bda3e08ab7321215594be249a9504d13b97
Jayden-official <pancafe198@gmail.com>
2024-06-27 18:16:54 +0300

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -2,12 +2,12 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-   if (!args[0]) throw `https://github.com/Khalid-official ✳💹*Enter serial number*\nVerify your serial number with the command...\n\n*${usedPrefix}nserial*`
+   if (!args[0]) throw `https://github.com/jayden-official ✳💹*Enter serial number*\nVerify your serial number with the command...\n\n*${usedPrefix}nserial*`
    let user = global.db.data.users[m.sender]
    let sn = createHash('md5').update(m.sender).digest('hex')
    if (args[0] !== sn) throw '⚠️ *Incorrect serial number*'
    user.registered = false
-   m.reply(`https://github.com/Khalid-official 👌Record deleted`)
+   m.reply(`https://github.com/jayden-official 👌Record deleted`)
 }
 handler.help = ['unreg <Serial Number>']
 handler.tags = ['rg']
