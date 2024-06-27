bf6767aea0dffb6da9b3367e88b41ecf41ee062d
Jayden-official <pancafe198@gmail.com>
2024-06-27 07:45:23 +0300

    Update internet-yts.js

--- plugins/internet-yts.js
+++ plugins/internet-yts.js
@@ -2,7 +2,7 @@ import yts from 'yt-search'
 import fs from 'fs'
 
 let handler = async (m, {conn, text }) => {
-  if (!text) throw 'https://github.com/Khalid-official ⚠️ *_What do you want me to look for on YouTube?_*'
+  if (!text) throw 'https://github.com/jayden-official ⚠️ *_What do you want me to look for on YouTube?_*'
   let results = await yts(text)
   let tes = results.all
   console.log(tes)
