406ef1b5c298853e174dfc5d9ae17463c537fc6d
Jayden-official <pancafe198@gmail.com>
2024-06-27 16:53:52 +0300

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -4,14 +4,14 @@ let handler = async(m, { conn, usedPrefix }) => {
      let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
      let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
     
-     m.reply(`https://github.com/Khalid-official 
+     m.reply(`https://github.com/jayden-official 
 ≡ *USERS BANNED*
 
-🐝 Total : *${users.length}*
+  Total : *${users.length}*
 
 ${users ? '\n' + users.map(([jid], i) => `
 ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
-🐝 ${jid}
+  ${jid}
 `.trim()).join('\n') : ''}
 `.trim())
 }
