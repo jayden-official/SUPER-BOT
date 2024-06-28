7bd0dd3879b56752baa828f97dbe942ee028c2b1
confrontertech <142077542+confronter@users.noreply.github.com>
2024-06-28 23:06:02 +0300

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -11,7 +11,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 𝗛𝗘𝗟𝗟𝗢, 𝗔𝗠 𝗔 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧, 𝗔𝗖𝗧𝗜𝗩𝗘 𝗙𝗢𝗥 ${muptime}\n\n ┃ 🍭 ᖴᖇOᗰ jAyDeN-TEᑕᕼᘔ`
+		let bio = `\n✳️𝔰𝔲𝔭𝔢𝔯 𝔟𝔬𝔱 𝔦𝔰 𝔯𝔲𝔫𝔫𝔦𝔫𝔤 ${muptime}\n\n ┃ 📲 ᖴᖇOᗰ jAyDeN ❌ℭ𝔬𝔫𝔣𝔯𝔬𝔫𝔱𝔢𝔯`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
