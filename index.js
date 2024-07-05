fc68300a77711878b6bd0526424bb26158574979
Jayden-official <pancafe198@gmail.com>
2024-07-05 09:19:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Bumblebee - Bot\nWhatsApp Bot MD', {
+say('super - Bot\nWhatsApp Bot MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot created by Khalid tech`, {
+say(`Bot created by jayden tech`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
